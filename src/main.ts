87879e3f73747a9c3cf94b892afd01904502cc28
Mikhail <mikhail.mudrogelov@gmail.com>
2025-11-30 21:36:25 +0300

    feat: implement swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { parse } from 'yaml';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -14,7 +17,14 @@ async function bootstrap() {
     }),
   );
 
+  const yamlFile = readFileSync('./doc/api.yaml', 'utf8');
+  const swaggerDocument = parse(yamlFile);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger documentation: http://localhost:${port}/doc`);
 }
 bootstrap();
