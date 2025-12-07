388b610dc165cd09f58cbee435189dd344b5b44c
Mikhail <mikhail.mudrogelov@gmail.com>
2025-12-07 19:58:50 +0300

    feat: rework artist entity

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArtistController } from './artist.controller';
 import { ArtistService } from './artist.service';
+import { Artist } from './entities/artist.entity';
+import { FavoriteArtist } from '../favorites/entities/favorite-artist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Artist, FavoriteArtist])],
   controllers: [ArtistController],
   providers: [ArtistService],
   exports: [ArtistService],
--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,5 +1,21 @@
-export interface Artist {
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Album } from '../../album/entities/album.entity';
+import { Track } from '../../track/entities/track.entity';
+
+@Entity('artist')
+export class Artist {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column({ default: false })
   grammy: boolean;
+
+  @OneToMany(() => Album, (album) => album.artist)
+  albums: Album[];
+
+  @OneToMany(() => Track, (track) => track.artist)
+  tracks: Track[];
 }
