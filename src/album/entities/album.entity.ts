87c2da5db021ac235ea4d15c57285d0bbfa6a46c
Mikhail <mikhail.mudrogelov@gmail.com>
2025-12-07 16:02:45 +0300

    feat: connect typeorm to album

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,6 +1,35 @@
-export interface Album {
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import { Artist } from '../../artist/entities/artist.entity';
+import { Track } from '../../track/entities/track.entity';
+
+@Entity('album')
+export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   year: number;
+
+  @Column({ type: 'uuid', nullable: true })
   artistId: string | null;
+
+  @ManyToOne(() => Artist, (artist) => artist.albums, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'artistId' })
+  artist: Artist;
+
+  @OneToMany(() => Track, (track) => track.album)
+  tracks: Track[];
 }
